9366027c7c6827b8dc98937f617306778300a01c
Justin Knutson <knutson.justin@gmail.com>
2016-12-07 18:56:38 -0800

    Added eslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,14 +3,14 @@ import gulp from 'gulp';
 import gulpSequence from 'gulp-sequence';
 import nodemon from 'gulp-nodemon';
 
-gulp.task('build:server', () => {
-  return gulp.src('./server.js')
+gulp.task('build:server', () =>
+  gulp.src('./server.js')
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist')),
+);
 
-gulp.task('nodemon', () => {
-  return nodemon({
+gulp.task('nodemon', () =>
+  nodemon({
     script: 'dist/server.js',
     // watch: /* Watch Things */,
     // ignore: /* Ignore Things */,
@@ -19,11 +19,12 @@ gulp.task('nodemon', () => {
     },
     verbose: true,
     ext: 'js html json',
-    env: { 'NODE_ENV': 'development' }
-  });
-});
+    env: { NODE_ENV: 'development' },
+  }),
+);
+
 
 gulp.task('default', gulpSequence(
   'build:server',
-  'nodemon'
+  'nodemon',
 ));
