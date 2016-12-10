50a7e8b2264fee0e521209a7e29f61eb68728cf3
Justin Knutson <knutson.justin@gmail.com>
2016-12-10 14:53:47 -0800

    Updated dependencies and port no.

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.set('port', process.env.PORT || 8080);
+
 app.get('/', (req, res) => {
   res.send('Follow the Great Dragon...');
 });
 
-app.listen(3000, () => {
-  console.log('Dragon listening on port 3000...');
+app.listen(app.get('port'), () => {
+  console.log(`Dragon listening on port ${app.get('port')}`);
 });
