b9fd53ba51107cafc1ad9482d87d5cd1aac9f90b
Justin Knutson <knutson.justin@gmail.com>
2017-02-23 09:55:23 -0800

    Added script folder with a query for users in the DB

---
+++ scripts/user-queries.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import '../config/dotenv.config';
+// import UserSchema from '../models/user';
+import TempUserSchema from '../models/temp-user';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI);
+
+// const User = mongoose.model('User', UserSchema);
+const TempUser = mongoose.model('TempUser', TempUserSchema);
+
+TempUser
+  .find({})
+  .select('code email')
+  .exec()
+  .then((users) => {
+    users.sort();
+    users.forEach((user) => {
+      console.log(user.email, user.code);
+    });
+    process.exit();
+  });
