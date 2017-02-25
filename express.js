40b2012c35cf21984328b60b4e46b7c1a1218333
Justin Knutson <knutson.justin@gmail.com>
2017-02-25 15:37:23 -0800

    Moved all routes to routes directory

--- express.js
+++ express.js
@@ -1,13 +1,9 @@
 import bodyParser from 'body-parser';
-import chalk from 'chalk';
 import cookieParser from 'cookie-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 import logger from 'morgan';
-import jade from 'jade';
-import mailgun from 'mailgun.config';
 import mongoSession from 'connect-mongodb-session';
-import mongoose from 'mongoose';
 import path from 'path';
 // import passport from 'passport';
 import session from 'express-session';
@@ -47,151 +43,9 @@ app.use(session({
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(router);
-
-/*
-app.get('/', (req, res) => {
-  res.render('index');
-});
-*/
-
 // app.use(passport.initialize());
 // app.use(passport.session());
 
-const User = mongoose.model('User');
-const TempUser = mongoose.model('TempUser');
-const codes = [
-  'TESTT',
-  '8QP47',
-  '3AN22',
-  'L75BH',
-  'Z4P9A',
-  '85J22',
-  'I44N7',
-  '4KP22',
-  '82243',
-  '29A7P',
-  'M3A22',
-  'TJKC3',
-  'AZYVX',
-  'L2BA3',
-  '9GQHT',
-  'PQBBK',
-  'PDHB6',
-  'KS6LA',
-  'DNI48',
-  '8MTQW',
-  'HSBVN',
-  '9SH4A',
-  'AV3AP',
-  'A5GRS',
-  'RS3UT',
-  'XKYY5',
-  'R9V5K',
-  'RN7JZ',
-  'HCL72',
-  '88425',
-  'N5R6Z',
-  'QRNF6',
-  'MXKTX',
-  'Q9N45',
-  'AWLMN',
-  '6FCNS',
-  'PYCH4',
-  '74G7W',
-  'RSJ32',
-  'F5W5G',
-  '9TRPX',
-  'PBKLA',
-];
-
-app.post('/code', (req, res) => {
-  const code = req.body._code;
-
-  if (codes.includes(code)) return res.sendStatus(200);
-
-  return res.sendStatus(404);
-});
-
-class BadRequestError extends Error {
-  constructor(message) {
-    super(message);
-
-    this.code = 400;
-  }
-}
-
-app.post('/register', (req, res) => {
-  const code = req.body._code;
-  const email = req.body._email;
-
-  if (codes.includes(code)) {
-    return User.findOne({ code })
-      .exec()
-      .then((user) => {
-        if (user) {
-          throw new BadRequestError('User already exists!');
-        }
-
-        return TempUser.findOneAndRemove({ code }).exec();
-      })
-      .then(() => {
-        console.log(chalk.yellow('Creating new temp user...'));
-        return TempUser.create({ code, email });
-      })
-      .then((tempUser) => {
-        console.log(chalk.green('Successfully created temp user:'), tempUser._id);
-        const html = jade.renderFile('./views/confirmation-email.jade', {
-          confirmurl: `http://${req.get('host')}/confirm/${tempUser.id}`,
-        });
-        return mailgun.messages().send({
-          from: 'Korea 2017 Portal <do-not-reply@followthegreatdragon.com>',
-          to: email,
-          subject: 'Please Confirm Your Email Address',
-          html,
-        });
-      })
-      .then((mail) => {
-        console.log(mail);
-        return res.sendStatus(200);
-      })
-      .catch((err) => {
-        const errCode = err.code || 500;
-
-        return res.status(errCode).send(err.message);
-      });
-  }
-
-  return res.status(404).send('Code invalid!');
-});
-
-app.get('/confirm/:id', (req, res) => {
-  const _id = req.params.id;
-  TempUser.findOne({ _id })
-    .exec()
-    .then((tempUser) => {
-      if (!tempUser) {
-        throw new BadRequestError('ID no longer valid');
-      }
-
-      return tempUser.remove();
-    })
-    .then((tempUser) => {
-      const email = tempUser.email;
-      const code = tempUser.code;
-
-      return User.create({ code, email });
-    })
-    .then((user) => {
-      console.log(user);
-      return res.send('Thank you! Your email has been confirmed. '
-          + 'Please check your inbox periodically for updates in 2017.');
-    })
-    .catch((err) => {
-      const errCode = err.code || 500;
-
-      return res.status(errCode).send(err.message);
-    });
-});
+app.use(router);
 
 export default app;
