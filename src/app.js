e164dbc55fec778c234e71672294dc7ccc751f5b
Justin Knutson <knutson.justin@gmail.com>
2017-03-25 15:50:28 -0700

    Added ui-bootstrap and an interruptive modal

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 
 import angular from 'angular';
 import ngRoute from 'angular-route';
+import uiModal from 'angular-ui-bootstrap/src/modal/index-nocss';
 import uiRouter from 'angular-ui-router';
 
 import components from './components';
@@ -11,18 +12,27 @@ import templates from './templates';
 
 angular.module('DragonApp', [
   ngRoute,
+  uiModal,
   uiRouter,
   components,
   routes,
   services,
   templates,
 ])
-.run(($transitions, $q) => {
+.run(($transitions, $q, $uibModal) => {
   $transitions.onStart({ to: 'korea.**' }, (trans) => {
     const $state = trans.router.stateService;
     const UserService = trans.injector().get('UserService');
 
-    return $q.when(UserService.getFail())
+    return $q.when(UserService.get())
+      .then((user) => {
+        if (!user.code) {
+          $uibModal.open({
+            template: '<h2>Test Modal!</h2>',
+            backdrop: 'static',
+          });
+        }
+      })
       .catch(() => $state.target('login'));
   });
 });
