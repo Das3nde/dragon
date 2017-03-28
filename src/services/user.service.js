270bc8815e2e401b37fd49b89138529d60a945fe
Justin Knutson <knutson.justin@gmail.com>
2017-03-28 12:29:55 -0700

    Removed comments and logs from user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,24 +1,11 @@
 export default class {
   constructor($http) {
     Object.assign(this, { $http });
-
-    /*
-    this.user = {
-      firstName: 'Justin',
-      lastName: 'Knutson',
-      fullName: 'Justin Knutson',
-      email: 'knutson.justin@gmail.com',
-      phone: '2537203662',
-      birthday: '06/23/1989',
-      inviteYear: '1989',
-    };
-    */
   }
 
   get() {
     return this.user || this.$http.get('/user')
       .then((user) => {
-        console.log(user);
         this.user = user;
         return this.user;
       });
