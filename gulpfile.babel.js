88d557a737c76ea774844fe9499f2f1bb139b77c
Justin Knutson <knutson.justin@gmail.com>
2017-03-30 18:49:39 -0700

    Added new scss gulp task and watchers for scss and jade

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,15 +2,31 @@ import angularTemplatecache from 'gulp-angular-templatecache';
 import babelify from 'babelify';
 import buffer from 'vinyl-buffer';
 import browserify from 'browserify';
+import concat from 'gulp-concat';
 import gulp from 'gulp';
 import gulpSequence from 'gulp-sequence';
 import jade from 'gulp-jade';
 import nodemon from 'gulp-nodemon';
 import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
+import sass from 'gulp-sass';
 import source from 'vinyl-source-stream';
 
 const jsExecMap = 'NODE_PATH=local_modules node -r ./dotenv.js';
+
+/**
+ * gulp build:css
+ *
+ * Transpile scss and bundle it into one
+ * styles.css in public directory
+ */
+
+gulp.task('build:css', () => gulp.src('./src/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('styles.css'))
+  .pipe(gulp.dest('./public'),
+));
+
 
 /**
  * gulp build:jade
@@ -59,6 +75,31 @@ gulp.task('build:app', () =>
 
 
 /**
+ * gulp watch:scss
+ *
+ * Watch for changes to scss files in the src directory
+ * and trigger the build:css task if necessary
+ */
+
+gulp.task('watch:scss', () => {
+  gulp.watch('./src/**/*.scss', ['build:css']);
+});
+
+
+/**
+ * gulp watch:jade
+ *
+ * Watch for changes to jade files in the src directory
+ * and trigger the build:jade task if necessary (also
+ * rebuild the app)
+ */
+
+gulp.task('watch:jade', () => {
+  gulp.watch('./src/**/*.jade', ['build:jade', 'build:app']);
+});
+
+
+/**
  * gulp nodemon
  *
  * Run a nodemon server (for local development)
@@ -67,13 +108,20 @@ gulp.task('build:app', () =>
 gulp.task('nodemon', () =>
   nodemon({
     script: 'server.js',
-    // watch: /* Watch Things */,
-    // ignore: /* Ignore Things */,
+    /*
+    watch: [
+      '!src/*',
+    ],
+    */
+    ignore: [
+      'src/*',
+      'public/*',
+    ],
     execMap: {
       js: jsExecMap,
     },
     verbose: true,
-    ext: 'js html json',
+    ext: 'js html json jade',
     env: { NODE_ENV: 'development' },
   }),
 );
@@ -85,7 +133,16 @@ gulp.task('nodemon', () =>
  * Compile angular templates and then build the angular application.
  */
 
-gulp.task('build', gulpSequence('build:jade', 'build:app'));
+gulp.task('build', gulpSequence('build:css', 'build:jade', 'build:app'));
+
+
+/**
+ * gulp watch
+ *
+ * Trigger all Watch tasks for development
+ */
+
+gulp.task('watch', ['watch:scss', 'watch:jade']);
 
 
 /**
@@ -96,5 +153,6 @@ gulp.task('build', gulpSequence('build:jade', 'build:app'));
 
 gulp.task('default', gulpSequence(
   'build',
+  'watch',
   'nodemon',
 ));
