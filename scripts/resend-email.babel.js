2ba93af9fe4796d044e561eeba4792359d4ad3b7
Justin Knutson <knutson.justin@gmail.com>
2017-04-06 12:31:31 -0700

    Added a send email script

--- scripts/resend-email.babel.js
+++ scripts/resend-email.babel.js
@@ -2,26 +2,25 @@ import jade from 'jade';
 import mailgun from 'config/mailgun';
 import mongoose from 'mongoose';
 
-import TempUserSchema from 'models/temp-user';
+import UserSchema from 'models/user';
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://heroku_t2qxw14d:5l7pdd231r4shlpts2a10biooc@ds033145.mongolab.com:33145/heroku_t2qxw14d');
+mongoose.connect('mongodb://localhost:27017/dragon');
 
-const TempUser = mongoose.model('TempUser', TempUserSchema);
+const User = mongoose.model('User', UserSchema);
 
-TempUser.find({})
-  .exec()
+User.find({}).exec()
   .then((users) => {
     users.forEach((user) => {
-      const html = jade.renderFile('./views/confirmation-email.jade', {
-        confirmurl: `http://www.followthegreatdragon.com/confirm/${user.id}`,
+      const html = jade.renderFile('./views/invite-email.jade', {
+        code: user.code,
       });
 
       mailgun.messages().send({
         from: 'Korea 2017 Portal <do-not-reply@followthegreatdragon.com>',
         to: user.email,
         bcc: 'knutson.justin@gmail.com',
-        subject: 'Please Confirm Your Email Address',
+        subject: 'Korea 2017 - New Information',
         html,
       })
       .then((mail) => {
